cb442fde9b0aa62c0b0558d5ce3910a08f3a8464
daniellindros <daniel.lindros@gmail.com>
2018-07-31 22:26:33 +0200

    WIP

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Index from '@/components/Index';
+import TodoItem from '@/components/TodoItem';
 export default {
-  name: 'App'
-}
+  name: 'App',
+  data() {
+    return {};
+  },
+  components: {
+    Index,
+    TodoItem
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ todo-app/src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
